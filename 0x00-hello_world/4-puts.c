f16fd2ba8be3207b3148f9c070af1019c39e4d3f
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-02-18 04:40:05 -0800

    starting c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program print a line with puts function
+ * Return: 0 (means success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
