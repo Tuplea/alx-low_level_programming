8d1a2aa057a331ef7582fec696d65d43ca5084ff
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-02-18 10:08:32 -0800

    running c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - This program print a line using the printf function
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+}
