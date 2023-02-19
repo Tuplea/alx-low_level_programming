9e3d9dd8d368c7771e32928d14620cb50611dbe8
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-02-18 16:00:34 -0800

    the last push

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - function that prints exact given statement
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
