7463935ed4a2a505951cf2a8f31d0b114a463d1a
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-02-19 08:15:41 -0800

    latest corrections

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 48, n < 58, n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints all possible combination
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
