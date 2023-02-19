34a1f7904512551fca2178cb7867f8c1ba1dd380
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-02-19 04:21:59 -0800

    alphabets cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints alphabets in lower then uppercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
