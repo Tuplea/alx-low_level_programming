c3ff5fe882cf7ca86845939bd39e4665ba6b954a
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-02-20 08:14:20 -0800

    new corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	if ((n % 10) > 5)
 	{
-i		printf("Last digit of %d is %d and is greater than 5\n",
+		printf("Last digit of %d is %d and is greater than 5\n",
 				n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
