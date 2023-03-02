990131a72d7252b9da81b4575851cf7b9f27de8a
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-03-01 23:55:46 -0800

    commit this

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,9 +11,9 @@ int main(void)
   /*
    * write your line of code here...
    * Remember:
-   * - you are not allowed to use a
+   * main - you are not allowed to use a
    * - you are not allowed to modify p
-   * - only one statement
+   * Return: Always 0.
    * - you are not allowed to code anything else than this line of code
    */
 	*(p + 5) = 98;
