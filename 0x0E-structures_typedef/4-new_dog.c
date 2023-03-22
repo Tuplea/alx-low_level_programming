5f42f0cee6e2b8a625101fb41b41a2a68d7ce870
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-03-21 22:09:00 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,9 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -30,8 +32,9 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
-	
+
 	while (src[len] != '\0')
 	{
 		len++;
@@ -56,10 +59,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
-	
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
