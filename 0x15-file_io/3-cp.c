86093de74bbf01259468c5a0a33593227d606065
Adeniyi Aderehinwo <adeniyiaderehinwo@gmail.com>
2023-04-10 03:21:17 -0700

    update this

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - ALlocates 1024 bytes for a buffer.
+ * create_buffer - allocates 1024 bytes for buffer.
  * @file: is the name of the file buffers storing chars
  * Return: pointer is returned to the newly allocated buffer.
  */
@@ -19,11 +19,28 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't write to%s\n", file);
+				"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
 	return (buffer);
+}
+
+/**
+ * close_file - file descriptors close
+ * @fd: is the closed descriptor
+ */
+void close_file(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
